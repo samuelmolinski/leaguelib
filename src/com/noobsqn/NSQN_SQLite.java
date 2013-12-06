bfe1a804c72c1ad6661ea04643ede10a995c4ee9
Samuel Molinski <sjmolinski@gmail.com>
2013-12-06 18:11:08 -0200

    + full sqlite print of 'items' table

--- src/com/noobsqn/NSQN_SQLite.java
+++ src/com/noobsqn/NSQN_SQLite.java
@@ -9,7 +9,7 @@ import java.io.File;
  */
 public class NSQN_SQLite {
 
-    public static void main( String args[] ){
+    public NSQN_SQLite(){
 
         String fileName = "sqlite/gameStats_en_US.sqlite";
 
@@ -23,8 +23,29 @@ public class NSQN_SQLite {
 
             stmt = c.createStatement();
             ResultSet rs = stmt.executeQuery( "SELECT * FROM items;" );
+
+            String[] columnName = this.getColumnNameArray(rs);
+            String[] columnType = this.getColumnTypeArray(rs);
+
+            for (int i = 0; i < columnName.length; i++){
+                //System.out.println(columnName[i]);
+            }
+
             while ( rs.next() ) {
-                int id = rs.getInt("id");
+                for (int i = 0; i < columnName.length; i++){
+                    //System.out.println(columnType[i]);
+                    if(columnType[i].equals("INTEGER")){
+                        System.out.println(columnName[i] + " " + String.valueOf(rs.getInt(columnName[i])));
+                    }
+                    if(columnType[i].equals("TEXT")){
+                        System.out.println(columnName[i] + " " + String.valueOf(rs.getString(columnName[i])));
+                    }
+                    if(columnType[i].equals("REAL")){
+                        System.out.println(columnName[i] + " " + String.valueOf(rs.getFloat(columnName[i])));
+                    }
+                }
+                System.out.println("");
+                /*int id = rs.getInt("id");
                 String  name = rs.getString("name");
                 int description  = rs.getInt("description");
                 String  price = rs.getString("price");
@@ -33,7 +54,7 @@ public class NSQN_SQLite {
                 System.out.println( "name = " + name );
                 System.out.println( "description = " + description );
                 System.out.println( "price = " + price );
-                System.out.println();
+                System.out.println();*/
             }
             rs.close();
             stmt.close();
@@ -43,5 +64,35 @@ public class NSQN_SQLite {
             System.exit(0);
         }
         System.out.println("Opened database successfully");
+    }
+    public String[] getColumnNameArray(ResultSet rs) {
+        String sArr[] = null;
+        try {
+            ResultSetMetaData rm = rs.getMetaData();
+            String sArray[] = new String[rm.getColumnCount()];
+            for (int ctr = 1; ctr <= sArray.length; ctr++) {
+                String s = rm.getColumnName(ctr);
+                sArray[ctr - 1] = s;
+            }
+            return sArray;
+        } catch (Exception e) {
+            System.out.println(e);
+            return sArr;
+        }
+    }
+    public String[] getColumnTypeArray(ResultSet rs) {
+        String sArr[] = null;
+        try {
+            ResultSetMetaData rm = rs.getMetaData();
+            String sArray[] = new String[rm.getColumnCount()];
+            for (int ctr = 1; ctr <= sArray.length; ctr++) {
+                String s = rm.getColumnTypeName(ctr);
+                sArray[ctr - 1] = s;
+            }
+            return sArray;
+        } catch (Exception e) {
+            System.out.println(e);
+            return sArr;
+        }
     }
 }
---
+++ src/com/noobsqn/NSQN_Test.java
@@ -0,0 +1,14 @@
+package com.noobsqn;
+
+import java.sql.*;
+
+/**
+ * Created by Samuel on 05/12/13.
+ */
+public class NSQN_Test {
+
+    public static void main( String args[] ){
+
+        NSQN_SQLite sql = new  NSQN_SQLite();
+    }
+}
