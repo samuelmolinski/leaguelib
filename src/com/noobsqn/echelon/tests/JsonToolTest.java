e5d7133d84e94da7baa3643ebebb22ce05f0ae31
Samuel Molinski <sjmolinski@gmail.com>
2013-12-26 14:27:14 -0200

    added a simple unit test to the test major changes to the jsontool

--- src/com/noobsqn/echelon/tests/JsonToolTest.java
+++ src/com/noobsqn/echelon/tests/JsonToolTest.java
@@ -1,6 +1,7 @@
 package com.noobsqn.echelon.tests;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.noobsqn.util.JsonTools;
@@ -13,23 +14,40 @@ public class JsonToolTest {
         JsonObject jor = new JsonObject();
 
         String str0 = "{}";
-        String str1 = "{\"spellBook\":{ \"dataVersion\":0, \"bookPages\":{ \"array\":[ { \"name\":\"test1\" }, { \"name\":\"test2\" } ] }, \"summonerId\":449946.0 }}";
-        String str2 = "{\"spellBook\":{ \"dataVersion\":0, \"bookPages\":{ \"array\":[ { \"name\":\"test2\" }, { \"name\":\"test3\" } ] }, \"someOtherId\":123456.0 }}";
-        String str3 = "{\"spellBook\":{ \"dataVersion\":0, \"bookPages\":{ \"array\":[{\"name\":\"test1\"},{\"name\":\"test2\"}] }, \"summonerId\":449946.0 }, \"summonerDefaultSpells\":{ \"dataVersion\":0, \"summonerDefaultSpellMap\":{ \"CLASSIC\":{ \"dataVersion\":0, \"spell2Id\":21, \"spell1Id\":4 }, \"FIRSTBLOOD\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ONEFORALL\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ARAM\":{ \"dataVersion\":0, \"spell2Id\":21, \"spell1Id\":4 }, \"TUTORIAL\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ODIN\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 } }, \"summonerId\":449946.0 }, \"summoner\":{ \"internalName\":\"zguli\", \"previousSeasonHighestTier\":\"DIAMOND\", \"acctId\":394846.0, \"name\":\"zGuli\", \"profileIconId\":15, \"sumId\":449946.0 }}";
-        String str4 = "{\"spellBook\":{ \"dataVersion\":0, \"bookPages\":{ \"array\":[{\"name\":\"test3\", \"otherProperty\": 1},{\"name\":\"test3\"}] }, \"summonerId\":449946.0 }, \"summonerDefaultSpells\":{ \"dataVersion\":0, \"summonerDefaultSpellMap\":{ \"CLASSIC\":{ \"dataVersion\":0, \"spell2Id\":21, \"spell1Id\":4 }, \"FIRSTBLOOD\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ONEFORALL\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ARAM\":{ \"dataVersion\":0, \"spell2Id\":21, \"spell1Id\":4 }, \"TUTORIAL\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ODIN\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 } }, \"summonerId\":449946.0 }, \"summoner\":{ \"internalName\":\"zguli\", \"previousSeasonHighestTier\":\"DIAMOND\", \"acctId\":394846.0, \"name\":\"zGuli\", \"profileIconId\":15, \"sumId\":449946.0 }}";
+        String str1 = "{\"dataVersion\":0, \"summonerId\":449946.0 }";
+        String str2 = "{\"spellBook\":{ \"dataVersion\":0, \"someOtherId\":123456.0 }}";
+        String str2_1 = "{\"spellBook\":{ \"dataVersion\":1, \"someOtherId\":0.0 }}";
+        String str2_2 = "{\"spellBook\":{ \"dataVersion\":1, \"someOtherId\":{\"dataVersion2\":0, \"summonerId2\":449946.0 } }}";
+        String str3 = "{\"spellBook\":{ \"dataVersion\":0, \"bookPages\":{ \"array\":[{\"name\":\"test1\"},{\"name\":\"test2\"}] }, \"summonerId\":449946.0 }}";
+        String str4 = "{\"spellBook\":{ \"dataVersion\":0, \"bookPages\":{ \"array\":[{\"name\":\"test1\"},{\"name\":\"test2\"}] }, \"summonerId\":449946.0 }, \"summonerDefaultSpells\":{ \"dataVersion\":0, \"summonerDefaultSpellMap\":{ \"CLASSIC\":{ \"dataVersion\":0, \"spell2Id\":21, \"spell1Id\":4 }, \"FIRSTBLOOD\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ONEFORALL\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ARAM\":{ \"dataVersion\":0, \"spell2Id\":21, \"spell1Id\":4 }, \"TUTORIAL\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ODIN\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 } }, \"summonerId\":449946.0 }, \"summoner\":{ \"internalName\":\"zguli\", \"previousSeasonHighestTier\":\"DIAMOND\", \"acctId\":394846.0, \"name\":\"zGuli\", \"profileIconId\":15, \"sumId\":449946.0 }}";
+        String str5 = "{\"spellBook\":{ \"dataVersion\":0, \"bookPages\":{ \"array\":[{\"name\":\"test3\", \"otherProperty\": 1},{\"name\":\"test3\"}] }, \"summonerId\":449946.0 }, \"summonerDefaultSpells\":{ \"dataVersion\":0, \"summonerDefaultSpellMap\":{ \"CLASSIC\":{ \"dataVersion\":0, \"spell2Id\":21, \"spell1Id\":4 }, \"FIRSTBLOOD\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ONEFORALL\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ARAM\":{ \"dataVersion\":0, \"spell2Id\":21, \"spell1Id\":4 }, \"TUTORIAL\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ODIN\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 } }, \"summonerId\":449946.0 }, \"summoner\":{ \"internalName\":\"zguli\", \"previousSeasonHighestTier\":\"DIAMOND\", \"acctId\":394846.0, \"name\":\"zGuli\", \"profileIconId\":15, \"sumId\":449946.0 }}";
+        //String str6 = "{\"spellBook\":{ \"dataVersion\":0, \"bookPages\":{ \"array\":[{\"name\":\"test3\", \"otherProperty\": 1},{\"name\":\"test3\"}] }, \"summonerId\":449946.0 }, \"summonerDefaultSpells\":{ \"dataVersion\":0, \"summonerDefaultSpellMap\":{ \"CLASSIC\":{ \"dataVersion\":0, \"spell2Id\":21, \"spell1Id\":4 }, \"FIRSTBLOOD\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ONEFORALL\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ARAM\":{ \"dataVersion\":0, \"spell2Id\":21, \"spell1Id\":4 }, \"TUTORIAL\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 }, \"ODIN\":{ \"dataVersion\":0, \"spell2Id\":14, \"spell1Id\":4 } }, \"summonerId\":449946.0 }, \"summoner\":{ \"internalName\":\"zguli\", \"previousSeasonHighestTier\":\"DIAMOND\", \"acctId\":394846.0, \"name\":\"zGuli\", \"profileIconId\":15, \"sumId\":449946.0 }}";
 
+        String arr0 = "[]";
+        String arr1 = "{\"array\":[] }";
+        String arr2 = "{\"array\":[{\"name\":\"test1\"},{\"name\":\"test2\"}] }";
+        String arr3 = "{\"bookPages\":{ \"array\":[{\"name\":\"test3\"},{\"name\":\"test4\"}] }}";
+        String arr4 = "{\"bookPages\":{ \"array\":[{\"name\":\"test2\"},{\"name\":\"test5\"}] }}";
         JsonTools jt = new JsonTools();
+        String[][] jors = {
+                //{str0, str1},
+                //{str0, str2},
+                //{str1, str2},
+                //{str2, str2_1},
+                //{str2, str2_2},
+                //{str0, str3},
+                //{str1, str3},
+                {arr0, str1},
 
-        JsonObject host = (JsonObject) jt.fromString(str0);
-        JsonObject addition1 = (JsonObject) jt.fromString(str1);
-        JsonObject addition2 = (JsonObject) jt.fromString(str2);
-        JsonObject addition3 = (JsonObject) jt.fromString(str3);
+        };
 
-        //jor = (JsonObject) jt.add(host, addition1);
-        jor = (JsonObject) jt.add(addition1, addition2);
-        //jor = (JsonObject) jt.add(host, addition);
+        for (int i = 0; i < jors.length; i++) {
+            JsonElement addend0 = (JsonElement) jt.fromString(jors[i][0]);
+            JsonElement addend1 = (JsonElement) jt.fromString(jors[i][1]);
+            jor = (JsonObject) jt.add(addend0, addend1);
+            System.out.println(jor.toString());
+        }
 
-        System.out.println(jor.toString());
         System.out.println("--END--");
     }
 
--- src/com/noobsqn/util/JsonTools.java
+++ src/com/noobsqn/util/JsonTools.java
@@ -2,6 +2,7 @@ package com.noobsqn.util;
 
 import com.google.gson.*;
 
+import java.lang.reflect.Type;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -10,11 +11,12 @@ import java.util.Map;
  */
 public class JsonTools {
 
-    final static int JSONNULL = 0;
-    final static int JSONPRIMITIVE = 1;
-    final static int JSONOBJECT = 2;
-    final static int JSONARRAY = 3;
-    final static int JSONELEMENT = 4;
+    final static int NULL = 0;
+    final static int JSONNULL = 1;
+    final static int JSONPRIMITIVE = 2;
+    final static int JSONOBJECT = 3;
+    final static int JSONARRAY = 4;
+    final static int JSONELEMENT = 5;
 
     public JsonElement add(JsonElement host, JsonElement addend){
         return add(host, addend, "");
@@ -22,83 +24,129 @@ public class JsonTools {
 
     public JsonElement add(JsonElement host, JsonElement addend, String key){
         int host_type = getType(host);
-        int addition_type = getType(addend);
+        int addend_type = getType(addend);
         switch (host_type){
-            case JSONPRIMITIVE :
-
-                //if(!key.isEmpty() && host.isJsonObject()){ host.getAsJsonObject().addProperty(key, addend.toString()); }
-                break;
             case JSONOBJECT :
-                addObject((JsonObject) host, (JsonObject) addend);
+                switch (addend_type){
+                    case JSONPRIMITIVE :
+                        if(!key.isEmpty()){ host.getAsJsonObject().addProperty(key, addend.toString()); }
+                        break;
+                    case JSONOBJECT :
+                        if(!key.isEmpty() && !host.getAsJsonObject().has(key)){
+                           // host.getAsJsonObject().add(key, null);
+                           addObject( host.getAsJsonObject(), (JsonObject) addend, key);
+                        } else if(!key.isEmpty()) {
+                            addObject( host.getAsJsonObject(), (JsonObject) addend, key);
+                        } else {
+                            addObject( host, (JsonObject) addend);
+                        }
+
+                        break;
+                    case JSONARRAY :
+                        addArray(host, (JsonArray) addend, key);
+                        break;
+                    default:// JSONNULL & JSONPRIMITIVE :
+                        break;
+                }
                 break;
             case JSONARRAY :
-                    addArray(host, (JsonArray) addend, key);
-                break;
-            default:// JSONNULL :
-                break;
-        }
-        //host.addProperty(entry.getKey(), String.valueOf(entry.getValue()));
-        //String code = array.getAsJsonPrimitive("code").getAsString();
-        return host;
-    }
-
-    public JsonElement addObject(JsonElement host, JsonObject addition){
-        for (Map.Entry<String,JsonElement> entry : addition.entrySet()) {
-            String value = entry.getValue().toString();
-            JsonElement njo = fromString(value);
-            /*if(null == host.get(entry.getKey())) {
-                //host = (JsonObject) mergeObject(host, njo, entry.getKey());
-            } else if (host.get(entry.getKey()).isJsonPrimitive()) {
-                host = (JsonObject) add(host, njo, entry.getKey());
-            } else { //if (host.get(entry.getKey()).isJsonArray()){
-                //JsonElement subHost = host.get(entry.getKey());
-                host = (JsonObject) add(host, njo, entry.getKey());
-            } else {
-                //host = add(subHost, njo, entry.getKey());
-            }*/
-            //JsonElement subHost = host.get(entry.getKey());
-            host = add(host.getAsJsonObject().get(entry.getKey()), njo, entry.getKey());
-        }
-        return host;
-    }
-    public JsonElement addObject(JsonObject host, JsonObject addition, String key){
-        //host = host.getAsJsonObject(key);
-        for (Map.Entry<String,JsonElement> entry : addition.entrySet()) {
-            String value = entry.getValue().toString();
-            JsonElement njo = fromString(value);
-            /*if(null == host.get(entry.getKey())) {
-                host = (JsonObject) mergeObject(host, njo, entry.getKey());
-            } else if (host.get(entry.getKey()).isJsonPrimitive()) {
-                host = (JsonObject) mergeObject(host, njo, entry.getKey());
-            } else { //if (host.get(entry.getKey()).isJsonArray()){
-                //JsonElement subHost = host.get(entry.getKey());
-                host = (JsonObject) mergeObject(host, njo, entry.getKey());
-            } else {
-                host = mergeObject(subHost, njo, entry.getKey());
-            }*/
-            //JsonElement subHost = host.get(entry.getKey());
-            host = (JsonObject) add(host, njo, entry.getKey());
-        }
-        return host;
-    }
-
-    public JsonElement addArray(JsonElement host, JsonArray addition){
-        return addArray(host, addition, "");
-    }
-
-    public JsonElement addArray(JsonElement host, JsonArray addition, String key){
-        int host_type = getType(host);
-        if((JSONOBJECT == host_type) && (!key.isEmpty())) {
-            host.getAsJsonObject().add(key, addition);
-        }
-        if(JSONARRAY == host_type){
-            Iterator itr = addition.iterator();
-            while(itr.hasNext()){
-                JsonElement add_njo = (JsonElement) itr.next();
-                if(!inArray(host.getAsJsonArray(), add_njo)){
-                    host.getAsJsonArray().add(add_njo);
+                switch (addend_type){
+                    case JSONPRIMITIVE :
+                        if(!key.isEmpty() && host.isJsonObject()){ host.getAsJsonArray().add(addend); }
+                        break;
+                    case JSONOBJECT :
+                        addObject(host, (JsonObject) addend);
+                        break;
+                    case JSONARRAY :
+                        addArray( host, (JsonArray) addend);
+                        break;
+                    default:// JSONNULL & JSONPRIMITIVE :
+                        break;
                 }
-            }
+                break;
+            default:// NULL & JSONNULL & JSONPRIMITIVE :
+                // Do nothing
+                break;
+        }
+        return host;
+    }
+
+    public JsonElement addObject(JsonElement host, JsonObject addend){
+        return addObject(host, addend, "");
+    }
+    public JsonElement addObject(JsonElement host, JsonObject addend, String key){
+        //host = host.getAsJsonObject(key);
+        int host_type = getType(host);
+        switch(host_type) {
+            case JSONELEMENT:
+                System.err.println("Can not pass Non Obj/Array JsonElement to addObject()");
+                break;
+            case JSONOBJECT:
+                for (Map.Entry<String,JsonElement> entry : addend.entrySet()) {
+                    //String value = entry.getValue().toString();
+                    JsonElement njo = fromString(entry.getValue().toString());
+                    int entry_type = getType(entry.getValue());
+                    switch(entry_type) {
+                        case JSONPRIMITIVE:
+                            if(JSONPRIMITIVE != getType(host.getAsJsonObject().get(entry.getKey()))){host.getAsJsonObject().add(entry.getKey(), null);}
+                            host.getAsJsonObject().add(entry.getKey(), entry.getValue());
+                            break;
+                        case JSONELEMENT: // should never happen
+                            /*if(null == host.getAsJsonObject().get(entry.getKey())){host.getAsJsonObject().add(entry.getKey(), null);}
+                            host = add(host, njo, entry.getKey());*/
+                            System.err.println("Addend entry can not be type JSONELEMENT");
+                            break;
+                        case JSONOBJECT:
+                            if(JSONOBJECT != getType(host.getAsJsonObject().get(entry.getKey()))){host.getAsJsonObject().add(entry.getKey(), new JsonObject());}
+                            host = add(host.getAsJsonObject().get(entry.getKey()), njo);
+                            break;
+                        case JSONARRAY:
+                            if(JSONARRAY !=  getType(host.getAsJsonObject().get(entry.getKey()))){host.getAsJsonObject().add(entry.getKey(), new JsonArray());}
+                            host = add(host.getAsJsonObject().get(entry.getKey()), njo, entry.getKey());
+                            break;
+                    }
+                }
+                break;
+            case JSONARRAY:
+                //when addeding obj to an array, if we want to do farther reaching comparison into the array, this is there to start
+                if(!inArray(host.getAsJsonArray(), addend)){
+                    host.getAsJsonArray().add(addend);
+                }
+                break;
+        }
+        return host;
+    }
+
+    public JsonElement addArray(JsonElement host, JsonArray addend){
+        return addArray(host, addend, "");
+    }
+
+    public JsonElement addArray(JsonElement host, JsonArray addend, String key){
+        int host_type = getType(host);
+        switch(host_type) {
+            case JSONPRIMITIVE:
+                System.err.println("Addend can not be type JSONPRIMITIVE");
+                break;
+            case JSONELEMENT: // should never happen
+                System.err.println("Addend can not be type JSONELEMENT");
+                break;
+            case JSONOBJECT:
+                if(key.isEmpty()){ key = "array"; }
+                if(JSONARRAY != getType(host.getAsJsonObject().get(key))) {
+                    host.getAsJsonObject().add(key, addend);
+                } else {
+                    host = add(host.getAsJsonObject().get(key), addend);
+                }
+                break;
+            case JSONARRAY:
+                Iterator itr = addend.iterator();
+                while(itr.hasNext()){
+                    JsonElement add_nje = (JsonElement) itr.next();
+                    if(!inArray(host.getAsJsonArray(), add_nje)){
+                        host.getAsJsonArray().add(add_nje);
+                    }
+                }
+                break;
         }
         return host;
     }
@@ -115,17 +163,21 @@ public class JsonTools {
     }
 
     public int getType(JsonElement je){
-        if(je.isJsonNull()){
-            return JSONNULL;
-        }
-        if(je.isJsonPrimitive()){
-            return JSONPRIMITIVE;
-        }
-        if(je.isJsonObject()){
-            return JSONOBJECT;
-        }
-        if(je.isJsonArray()){
-            return JSONARRAY;
+        if(null != je){
+            if(je.isJsonNull()){
+                return JSONNULL;
+            }
+            if(je.isJsonPrimitive()){
+                return JSONPRIMITIVE;
+            }
+            if(je.isJsonObject()){
+                return JSONOBJECT;
+            }
+            if(je.isJsonArray()){
+                return JSONARRAY;
+            }
+        } else{
+            return NULL;
         }
         return JSONELEMENT;
     }
